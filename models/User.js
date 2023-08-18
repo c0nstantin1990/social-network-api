23874b2b02542a54ad1554c10864e01e3c5dcbdf
c0nstantin1990 <118696575+c0nstantin1990@users.noreply.github.com>
2023-08-18 09:09:58 -0400

    created user model

---
+++ models/User.js
@@ -0,0 +1,36 @@
+const { Schema, model } = require("mongoose");
+
+// Define the user schema
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, "Must match an email address!"],
+    },
+    thoughts: [{ type: Schema.Types.ObjectId, ref: "thought" }],
+    friends: [{ type: Schema.Types.ObjectId, ref: "user" }],
+  },
+  {
+    toJSON: { virtuals: true },
+    id: false,
+  }
+);
+
+// Define a virtual property 'friendCount'
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+// Create User model using userSchema
+const User = model("user", userSchema);
+
+// Export the User model
+module.exports = User;
